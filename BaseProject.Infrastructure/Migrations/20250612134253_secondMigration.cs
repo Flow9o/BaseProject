e82bead0274f3b45af91c8aa1802aee729a47872
Flow9o <krugflo@gmx.de>
2025-06-13 08:50:30 +0200

    inital commit

---
+++ BaseProject.Infrastructure/Migrations/20250612134253_secondMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookmarks_Cars_CarId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookmarks_CarId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropColumn(
+                name: "CarId",
+                table: "Bookmarks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CarId",
+                table: "Bookmarks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_CarId",
+                table: "Bookmarks",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookmarks_Cars_CarId",
+                table: "Bookmarks",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id");
+        }
+    }
+}
